e57ec112aeb74a05e3e81a7c44684bba20e2547d
ChanikyaVarmaNalla <nchanikyavarma04@gmail.com>
2023-09-21 15:00:10 +0530

    Task 3

--- Sep Task 3.py
+++ Sep Task 3.py
@@ -3,12 +3,15 @@ import re
 class EmailExtractor:
     def __init__(self, file_path):
         self.file_path = file_path
-        self.email_pattern = r'[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}'
+        self.email_pattern = r'[A-Za-z][A-Za-z0-9._-]+@[A-Za-z0-9._-]+\.[A-Za-z]{2,}'
 
     def extract_emails(self):
+        emails = []
         with open(self.file_path, 'r') as file:
-            text = file.read()
-            return re.findall(self.email_pattern, text)
+            for line in file:
+                line_emails = re.findall(self.email_pattern, line)
+                emails.extend(line_emails)
+        return emails
 
 if __name__ == "__main__":
     email_extractor = EmailExtractor('document.txt')
