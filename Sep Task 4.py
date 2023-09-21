fcebe9f51d75fe85f64dd7ffd104376743537638
ChanikyaVarmaNalla <nchanikyavarma04@gmail.com>
2023-09-21 17:47:20 +0530

    Task 4

---
+++ Sep Task 4.py
@@ -0,0 +1,38 @@
+import json
+
+class Employee:
+    def __init__(self, employee):
+        self.employee = employee
+        print(f"I am in {self.__class__.__name__} Init")
+
+    def get_info(self):
+        return f"Employees of XYZ Company: {self.employee}"
+
+class Manager(Employee):
+    def __init__(self, employee):
+        super().__init__(employee)
+
+    def get_info(self):
+        manager_info = [emp for emp in self.employee["employees"] if emp["position"] == "Manager"]
+        header = "Employees of XYZ Company who are Managers:"
+        return f"{header}  {[i['name'] for i in manager_info]} are managers"
+
+class Developer(Employee):
+    def __init__(self, employee):
+        super().__init__(employee)
+
+    def get_info(self):
+        developer_info = [emp for emp in self.employee["employees"] if emp["position"] == "Developer"]
+        header = "Employees of XYZ Company who are Developers:"
+        return f"{header}  {[i['name'] for i in developer_info]} are developers"
+
+with open("employee_data.json", "r+") as rd:
+    data = json.load(rd)
+
+# Employees who are Managers
+mana = Manager(data)
+print(mana.get_info())
+
+# Employees who are Developers
+deve = Developer(data)
+print(deve.get_info())
